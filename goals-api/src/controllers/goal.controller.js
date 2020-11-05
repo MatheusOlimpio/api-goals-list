75e349edf9c102dd9d5dc903700a97b2de7954c2
Matheus Olimpio de Oliveira <mah.oliver_crz@hotmail.com>
2020-11-04 21:27:14 -0300

    Adicionando função para listar Goals

--- goals-api/src/controllers/goal.controller.js
+++ goals-api/src/controllers/goal.controller.js
@@ -6,12 +6,13 @@
 
 const db = require("../config/database");
 
+// =>  Metodo responsavel por criar uma nova 'Goal'
 exports.createGoal = async (req, res) => {
   console.log(req.body);
   const { goal, init_date, final_date, id_user } = req.body;
   const current_date = new Date(); 
   const goal_registration = `${current_date.getFullYear()}-${current_date.getMonth()}-${current_date.getDay()}`;
-  db('goals').insert({goal, init_date, final_date, id_user, goal_registration}).then(function (result) {
+  await db('goals').insert({goal, init_date, final_date, id_user, goal_registration}).then(function (result) {
     res.status(201).send({ 
       message: 'Goal added success',
       body: {
@@ -20,3 +21,12 @@ exports.createGoal = async (req, res) => {
     });     // respond back to request
   })
 };
+
+// =>  Metodo responsavel por listar as 'Goals'
+exports.listAllGoals = async (req, res) => {
+  const { id_user } = req.body;
+
+  await db('goals').where({id_user}).then(goals => {
+    res.status(200).send(goals);     // respond back to request
+  });
+}
--- goals-api/src/routes/goal.routes.js
+++ goals-api/src/routes/goal.routes.js
@@ -10,14 +10,14 @@
 
  // => Definindo Rotas do CRUD - 'Metas'
 
-//  // => Rota para visualizar as 'Metas': (GET): localhost:3000/api/goals
+//  // => Rota para visualizar as 'Goals' do usuario: (GET): localhost:3000/api/goals
+router.get('/goals', goalController.listAllGoals);
 
-
- // => Rota para cadastrar uma 'Meta': (POST): localhost:3000/api/goals
+ // => Rota para cadastrar uma 'Goal': (POST): localhost:3000/api/goals
 router.post('/goals', goalController.createGoal);
-//  // => Rota para editar uma 'Meta': (PUT): localhost:3000/api/goals
+//  // => Rota para editar uma 'Goal': (PUT): localhost:3000/api/goals
 
-//  // => Rota para excluir uma 'Meta': (DELETE): localhost:3000/api/goals
+//  // => Rota para excluir uma 'Goal': (DELETE): localhost:3000/api/goals
 
 
 
